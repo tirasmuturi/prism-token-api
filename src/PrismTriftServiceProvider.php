c122e66c6e07a2d9f3121fa347b3b4756e24641b
tirasmuturi <tiras.muturi@radioafricagroup.co.ke>
2021-11-22 20:46:47 +0300

    Prism Files to Composer project

---
+++ src/PrismTriftServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+class PrismTriftServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->package('', __DIR__);
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+
+}
