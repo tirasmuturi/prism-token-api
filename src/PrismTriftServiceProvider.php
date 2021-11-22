95831f5371dfa52e9e60a8bd21b8b8132565420f
tirasmuturi <tiras.muturi@radioafricagroup.co.ke>
2021-11-22 21:56:28 +0300

    A little refactoring

--- src/PrismTriftServiceProvider.php
+++ src/PrismTriftServiceProvider.php
@@ -20,7 +20,7 @@ class PrismTriftServiceProvider extends ServiceProvider {
      */
     public function register()
     {
-        $this->package('Tirasmuturi', 'PrismTokenApi', __DIR__);
+        $this->package(__DIR__);
     }
 
     /**
